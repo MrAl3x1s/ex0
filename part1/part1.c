a1f7f835a8799b2697f1bcbd28e6ee575721540e
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-13 14:12:21 +0300

    Change return values to those reserved in general

--- part1/part1.c
+++ part1/part1.c
@@ -2,9 +2,18 @@
 
 int main(void){
 // necessary for letting the Clion debugger print correctly and not get stuck.
-#ifndef NDEBUG
-    setbuf(stdout, 0);
-#endif
+    #ifndef NDEBUG
+        setbuf(stdout, 0);
+    #endif
+    printf("Enter size of input:\n");
+    int input;
+    if ((input = scanf("%d")) == 0) {
+        return RETURN_ERROR;
+    }
+    if (input < MIN_SIZE) {
+        printf("Invalid size\n");
+        return RETURN_ERROR;
+    }
 
-    return 0;
+    return RETURN_SUCCESS;
 }
