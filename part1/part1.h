4e00ef4428b41d740ebda9e9f44777c6e701c89a
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-13 14:11:56 +0300

    Remove inclusions moved to general
    And include them through general.

--- part1/part1.h
+++ part1/part1.h
@@ -1,16 +1,6 @@
+#include "General.h"
 // function declarations.
 #ifndef PART1_H
     #define PART1_H
     #define MIN_SIZE 1
-    #define RETURN_ERROR (-1)
-    #define RETURN_SUCCESS 0
 #endif //PART1_H
-// standard libraries.
-#ifndef STDIO_H
-    #define STDIO_H
-    #include <stdio.h>
-#endif //STDIO_H
-#ifndef STDLIB_H
-    #define STDLIB_H
-    #include <stdlib.h>
-#endif //STDLIB_H
