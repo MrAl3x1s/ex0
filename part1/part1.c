8659bf42fb70527d8d143eaf7a4a4ee84ddd7b5e
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-13 13:37:25 +0300

    Create basic structure for part 1
    Include standard libraries in header, guarded by macros to prevent double inclusion. Add fix for Clion debugger into main.

---
+++ part1/part1.c
@@ -0,0 +1,10 @@
+#include "part1.h"
+
+int main(void){
+// necessary for letting the Clion debugger print correctly and not get stuck.
+#ifndef NDEBUG
+    setbuf(stdout, 0);
+#endif
+
+    return 0;
+}
---
+++ part1/part1.h
@@ -0,0 +1,14 @@
+// function declarations.
+#ifndef PART1_H
+    #define PART1_H
+
+#endif //PART1_H
+// standard libraries.
+#ifndef STDIO_H
+    #define STDIO_H
+    #include <stdio.h>
+#endif //STDIO_H
+#ifndef STDLIB_H
+    #define STDLIB_H
+    #include <stdlib.h>
+#endif //STDLIB_H
