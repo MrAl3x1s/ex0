6eb5deb79833ab440a125e4e2d5c341f7e742b17
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-13 13:44:47 +0300

    Create integer macros
    For legal and illegal inputs and return values from main.

--- part1/part1.h
+++ part1/part1.h
@@ -1,7 +1,9 @@
 // function declarations.
 #ifndef PART1_H
     #define PART1_H
-
+    #define MIN_SIZE 1
+    #define RETURN_ERROR (-1)
+    #define RETURN_SUCCESS 0
 #endif //PART1_H
 // standard libraries.
 #ifndef STDIO_H
