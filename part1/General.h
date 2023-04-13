dc014703f9eb90c1bf18e2e8f69c54c4679bc6f7
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-13 14:09:49 +0300

    Turn General into common header
    Remove any function declarations, and leave inclusions of commonly used standard libraries and constants, such as maximal sizes of strings, and return values of main.

--- part1/General.h
+++ part1/General.h
@@ -1,26 +1,28 @@
-#ifndef STDIO_H
-    #define STDIO_H
-    #include <stdio.h>
-#endif
-#ifndef STDBOOL_H
-	#define STDBOOL_H
-	#include <stdbool.h>
-#endif
-#ifndef STDLIB_H
-	#define STDLIB_H
-	#include <stdlib.h>
-#endif
-#ifndef _GNU_SOURCE
-	#define _GNU_SOURCE
-#endif
+// general header file, with inclusions that are useful to almost any program.
 #ifndef GENERAL_H
-	#define GENERAL_H
-	void end(void);
-	bool readLine(char**);
-	bool readInt(int*);
-	bool readChar(char*);
+#define GENERAL_H
+    // standard libraries.
+    #ifndef STDIO_H
+        #define STDIO_H
+        #include <stdio.h>
+    #endif
+    #ifndef STDBOOL_H
+        #define STDBOOL_H
+        #include <stdbool.h>
+    #endif
+    #ifndef STDLIB_H
+        #define STDLIB_H
+        #include <stdlib.h>
+    #endif
+    // reserved max sizes.
+    #ifndef MAX_STR_SIZE
+        #define MAX_STR_SIZE 256
+    #endif
+    // values to return from main.
+    #ifndef RETURN_SUCCESS
+        #define RETURN_SUCCESS 0
+    #endif
+    #ifndef RETURN_ERROR
+        #define RETURN_ERROR (-1)
+    #endif
 #endif
-#ifndef MAX_STR_SIZE
-	#define MAX_STR_SIZE 256
-#endif
-
