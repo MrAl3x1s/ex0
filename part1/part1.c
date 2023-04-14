fe7449d0ab32d13d912a87c0bfb11481bf2577ad
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-14 20:40:31 +0300

    Record and store numbers
    TODO: implement with fscanf() instead.

--- part1/part1.c
+++ part1/part1.c
@@ -13,8 +13,29 @@ int main(void){
         return RETURN_SUCCESS;
     }
 
+    // allocate dynamic array to store all numbers.
     int *numbers = (int*) calloc(size, sizeof(int));
+    #ifndef NDEBUG
+    printf("\n[* Created pointer at address: %d]\n", &numbers);
+    #endif
+    // record every number in turn.
+    printf("Enter numbers:\n");
+    // clean input stream.
+    scanf("%*s");
+    for (int i = 0; i < size; i++) {
+        if (scanf("%d\n", &numbers[i]) < 1) {
+            printf("Invalid number\n");
+            return RETURN_SUCCESS;
+        }
+        #ifndef NDEBUG
+        printf("\n[* Recorded number: %d]\n", numbers[i]);
+        #endif
+    }
+    free(numbers);
 
+    #ifndef NDEBUG
+    printf("\n[* Freed pointer at address: %d]\n", &numbers);
+    #endif
 
     return RETURN_SUCCESS;
 }
