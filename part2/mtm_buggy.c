1575981a5e5942fa47805c0328c775aad7968a75
MrAl3x1s <al3x.k0ndratiev@gmail.com>
2023-04-15 12:38:41 +0300

    Fix sortStrings();
    Eliminate chance of an endless loop.

--- part2/mtm_buggy.c
+++ part2/mtm_buggy.c
@@ -54,7 +54,8 @@ void sortStrings(char** strings, int size) {
 	while (changed) {
 		changed = false;
 		for (int i = 0; i < size - 1; i++) {
-			if (strcmp(strings[i], strings[i + 1]) >= 0) {
+            // leaving the option of changing strings with no difference creates an endless loop.
+			if (strcmp(strings[i], strings[i + 1]) > 0) {
 				swap(&strings[i], &strings[i + 1]);
 				changed = true;
 			}
